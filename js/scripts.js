2693b5a03005e2322d7818e34aa800f80e203793
VyacheslavVolkhin <vv@htmlworker.com>
2024-11-04 17:59:20 +0400

    add

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,25 @@
 document.addEventListener("DOMContentLoaded", function() {
 
 
+
+	//button scroll up
+	let buttonUp = document.querySelector(".js-btn-up");
+	buttonUp.addEventListener('click', function(e) {
+		window.scrollTo({top: 0, behavior: 'smooth'});
+		e.preventDefault()
+		e.stopPropagation()
+	})
+
+	//button scroll 
+	document.querySelectorAll('.js-anchor').forEach(anchor => {
+		anchor.addEventListener('click', function (e) {
+			e.preventDefault();
+			document.querySelector(this.getAttribute('href')).scrollIntoView({
+				behavior: 'smooth'
+			});
+		});
+	});
+
 	//files add
 	const fileBlocks = document.querySelectorAll('.js-field-file');
 
@@ -45,8 +64,24 @@ document.addEventListener("DOMContentLoaded", function() {
 	}
 
 
-	//btn tgl
+	//show message add cart
+	const addButton = document.querySelector('.js-show-message-cart');
+	const popupBox = document.querySelector('.popup-box#popup-message-cart');
+
+	if (addButton) {
+		addButton.addEventListener('click', function() {
+			popupBox.classList.add('active');
+	
+			setTimeout(function() {
+				popupBox.classList.remove('active');
+			}, 5000);
+		});
+	}
+
+
+	//btn tgl and add
 	let tglButtons = document.querySelectorAll('.js-btn-tgl')
+	let addButtons = document.querySelectorAll('.js-btn-add')
 	for (i = 0;i < tglButtons.length;i++) {
 		tglButtons[i].addEventListener('click', function(e) {
 			this.classList.contains('active') ? this.classList.remove('active') : this.classList.add('active')
@@ -54,6 +89,23 @@ document.addEventListener("DOMContentLoaded", function() {
 			return false
 		})
 	}
+	for (i = 0;i < addButtons.length;i++) {
+		addButtons[i].addEventListener('click', function(e) {
+			if (!this.classList.contains('active')) {
+				this.classList.add('active');
+				if (this.classList.contains('js-show-message-cart')) {
+					document.querySelector('#popup-message-cart').classList.add('active');
+			
+					setTimeout(function() {
+						document.querySelector('#popup-message-cart').classList.remove('active');
+					}, 5000);
+				}
+				e.preventDefault()
+				return false
+			}
+		})
+	}
+	
 
 	//fancybox
 	Fancybox.bind("[data-fancybox]", {
@@ -166,6 +218,57 @@ document.addEventListener("DOMContentLoaded", function() {
 	})
 
 
+	//js tabs
+	const tabsNav = document.querySelectorAll('.js-tabs-nav')
+	const tabsBlocks = document.querySelectorAll('.js-tab-block')
+	const tabsButtonTitle = document.querySelectorAll('.js-tab-title')
+	const tabsButtonContent = document.querySelectorAll('.js-tab-content')
+	function tabsActiveStart() {
+		for (iTab = 0; iTab < tabsBlocks.length; iTab++) {
+			if (tabsBlocks[iTab].classList.contains('active')) {
+				tabsBlocks[iTab].classList.remove('active')
+			}
+		}
+		for (i = 0; i < tabsNav.length; i++) {
+			let tabsNavElements = tabsNav[i].querySelectorAll('[data-tab]')
+			for (iElements = 0; iElements < tabsNavElements.length; iElements++) {
+				if (tabsNavElements[iElements].classList.contains('active')) {
+					let tabsNavElementActive = tabsNavElements[iElements].dataset.tab
+					for (j = 0; j < tabsBlocks.length; j++) {
+						if (tabsBlocks[j].dataset.tab.toString().indexOf(tabsNavElementActive) > -1) {
+							console.log(tabsBlocks[j].dataset.tab.toString().indexOf(tabsNavElementActive))
+							tabsBlocks[j].classList.add('active')
+						}
+					}
+				}
+			}
+		}
+		
+	}
+	for (i = 0; i < tabsButtonTitle.length; i++) {
+		tabsButtonTitle[i].addEventListener('click', function (e) {
+			this.classList.toggle('active')
+			e.preventDefault()
+			e.stopPropagation()
+			return false
+		})
+	}
+	for (i = 0; i < tabsNav.length; i++) {
+		tabsNav[i].addEventListener('click', function (e) {
+			if (e.target.closest('[data-tab]')) {
+				let tabsNavElements = this.querySelector('[data-tab].active')
+				tabsNavElements ? tabsNavElements.classList.remove('active') : false
+				e.target.closest('[data-tab]').classList.add('active')
+				tabsActiveStart()
+				e.preventDefault()
+				e.stopPropagation()
+				return false
+			}
+		})
+	}
+	tabsActiveStart()
+
+
 
 	// Popups
 	let popupCurrent;
@@ -236,6 +339,23 @@ document.addEventListener("DOMContentLoaded", function() {
 	});
 
 
+	//slider accessories
+	const swiperSliderAccessories = new Swiper('.slider-accessories .swiper', {
+		loop: false,
+		slidesPerView: 'auto',
+		spaceBetween: 0,
+		autoHeight: true,
+		speed: 400,
+		pagination: false,
+		autoplay: false,
+		navigation: {
+			nextEl: '.btn-action-ico.ico-arrow.ico-arrow-next.button-slider-accessories-next',
+			prevEl: '.btn-action-ico.ico-arrow.ico-arrow-prev.button-slider-accessories-prev',
+		},
+	
+	});
+
+
 	//slider brands
 	const swiperSliderBrands = new Swiper('.slider-brands .swiper', {
 		loop: true,
@@ -281,6 +401,22 @@ document.addEventListener("DOMContentLoaded", function() {
 	
 	});
 
+	//slider reviews content
+	const swiperSliderReviewsContent = new Swiper('.slider-reviews-content .swiper', {
+		loop: true,
+		slidesPerView: 'auto',
+		spaceBetween: 0,
+		autoHeight: false,
+		speed: 400,
+		pagination: false,
+		autoplay: false,
+		navigation: {
+			nextEl: '.btn-action-ico.ico-arrow.ico-arrow-next.button-slider-reviews-content-next',
+			prevEl: '.btn-action-ico.ico-arrow.ico-arrow-prev.button-slider-reviews-content-prev',
+		},
+	
+	});
+
 
 	
 	
@@ -314,6 +450,22 @@ document.addEventListener("DOMContentLoaded", function() {
 		},
 	
 	});
+	//slider video content
+	const swiperSliderVideoContent = new Swiper('.slider-video-content .swiper', {
+		loop: false,
+		slidesPerView: 'auto',
+		spaceBetween: 0,
+		autoHeight: true,
+		speed: 400,
+		pagination: false,
+		autoplay: false,
+		initialSlide: 2,
+		navigation: {
+			nextEl: '.btn-action-ico.ico-arrow.ico-arrow-next.button-slider-video-content-next',
+			prevEl: '.btn-action-ico.ico-arrow.ico-arrow-prev.button-slider-video-content-prev',
+		},
+	
+	});
 
 
 	//slider topActions
@@ -339,8 +491,143 @@ document.addEventListener("DOMContentLoaded", function() {
 	});
 	
 	
+
+
+	//slider media thumbs preview
+	const swiperMediaPreview = new Swiper(".slider-media-thumbs .swiper",
+	{
+	  loop: false,
+	  slidesPerView: 4,
+	  spaceBetween: 0,
+	  threshold: 5,
+	  watchSlidesVisibility: true,
+	  watchSlidesProgress: true,
+	  freeMode: false,
+	  navigation: {
+		nextEl: ".button-slider-media-thumbs-next",
+		prevEl: ".button-slider-media-thumbs-prev",
+	},
+	});
+	
+	//slider media thumbs main
+	const swiperMediaMain = new Swiper(".slider-media-main .swiper",
+	{
+	  loop: false,
+	  slidesPerView: 1,
+	  spaceBetween: 0,
+	  autoHeight: true,
+	  speed: 400,
+	  threshold: 5,
+	  freeMode: false,
+	  watchSlidesProgress: true,
+	  navigation: false,
+	  pagination: {
+		clickable: true,
+		},
+	  thumbs: {
+		swiper: swiperMediaPreview,
+		},
+	});
+
+
+	//slider compare
+	const swiperSliderCompare = new Swiper('.slider-compare .swiper', {
+		loop: false,
+		slidesPerView: 'auto',
+		spaceBetween: 0,
+		autoHeight: false,
+		speed: 400,
+		pagination: false,
+		autoplay: false,
+		navigation: {
+			nextEl: '.btn-action-ico.ico-arrow.ico-arrow-next.button-slider-compare-next',
+			prevEl: '.btn-action-ico.ico-arrow.ico-arrow-prev.button-slider-compare-prev',
+		},
+	
+	});
+
+
+	//slider tiles
+	const swiperSliderTiles = new Swiper('.slider-tiles .swiper', {
+		loop: false,
+		slidesPerView: 2,
+		spaceBetween: 0,
+		autoHeight: false,
+		speed: 400,
+		pagination: false,
+		autoplay: false,
+		navigation: {
+			nextEl: '.btn-action-ico.ico-arrow.ico-arrow-next.button-slider-tiles-next',
+			prevEl: '.btn-action-ico.ico-arrow.ico-arrow-prev.button-slider-tiles-prev',
+		},
+		breakpoints: {
+			768: {
+				slidesPerView: 4,
+			},
+			1024: {
+				slidesPerView: 5,
+			},
+			1100: {
+				slidesPerView: 6,
+			},
+		},
+	
+	});
 	
 })
 
+
+
+
+
+//select toggle content visibility
+document.addEventListener("DOMContentLoaded", function () {
+	const inputs = document.querySelectorAll(
+	  "input[data-content], input[data-content-check], input[data-content-uncheck]"
+	);
+  
+	inputs.forEach(function (input) {
+	  toggleContent(input);
+	  });
+  
+	inputs.forEach((input) => {
+	  input.addEventListener("click", function () {
+		document.querySelectorAll(".frm-content").forEach((content) => {
+		  content.classList.remove("active");
+			  });
+  
+		inputs.forEach(toggleContent);
+		  });
+	  });
+  
+	document.querySelectorAll(".btn[data-content]").forEach((button) => {
+	  button.addEventListener("click", function () {
+		let dataContent = this.getAttribute("data-content");
+		this.disabled = true;
+		document
+		  .querySelectorAll('.frm-content[data-content="' + dataContent + '"]')
+		  .forEach((content) => {
+			content.classList.add("active");
+			  });
+		return false;
+		  });
+	  });
+  
+	function toggleContent(input) {
+	  let selectContent;
+	  if (input.checked) {
+		selectContent =
+		  input.getAttribute("data-content-check") ||
+		  input.getAttribute("data-content");
+		  } else {
+		selectContent = input.getAttribute("data-content-uncheck");
+		  }
+	  document
+		.querySelectorAll('.frm-content[data-content="' + selectContent + '"]')
+		.forEach((content) => {
+		  content.classList.add("active");
+		  });
+	  }
+  });
 
 
